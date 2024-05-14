dd9e09aedafb5171fe70b14170d9840b815a9f41
d3vCode0 <168995040+d3vCode0@users.noreply.github.com>
2024-05-14 01:28:51 +0000

    Update ExampleProvider.kt

--- ExampleHere/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleHere/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,9 +1,6 @@
 package com.example
 
 import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.cloudstream3.utils.ExtractorLink
-// import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 import org.jsoup.nodes.Element
 
 
@@ -58,60 +55,8 @@ class ExampleAPi : MainAPI() {
         }
     }
 
-    override suspend fun search(query: String): List<SearchResponse> {
-        val document = app.get("${mainUrl}?s=${query}").document
-        return document.select("div.search div.animes").mapNotNull {
-            it.toSearchResponse()
-        }
-    }
 
-    override suspend fun load(url: String): LoadResponse? {
-        val document = app.get(url).document
+    
 
-        val title = document.selectFirst("div.title h1")?.text()?.trim() ?: return null
-        val href = fixUrlNull(document.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = document.selectFirst("a")?.attr("data-src") ?: return null
-        val comingSoon = true
-
-        return newAnimeLoadResponse(title, href, TvType.Anime, comingSoon) {
-            // this.engName = eng // String
-            // this.japName = jap // String
-            // this.name = title // String
-            // this.url = href // String
-            // this.apiName = this@ExampleAPi.name
-            // this.type = TvType.Anime // TvType
-            this.posterUrl = posterUrl // String
-            // this.year = year // Int
-            // this.episodes = episodes // Ex: MutableMap<DubStatus, List<Episode>> = MutableMapOf()
-            // this.showStatus = showStatus // Ex ShowStatus.Companion Or ShowStatus.Ongoing
-            // this.plot = plot // String
-            // this.tags = tags // List Ex: document.select("div.genres a").map { it.text() }
-            // this.synonyms = synonyms // List
-            // this.rating = rating // Int
-            // this.duration = duration // Int
-            // this.trailers = trailers // String Ex: addTrailer(trailers)
-            // this.recommendations = recommendations // List<SearchResponse> Ex: document.select("div.animes div.recommendations").map { it.toSearchResponse() }
-            // this.actors = actors // List<ActorData> Ex: .mapNotNull { ActorData(actor =  Actor(name, image), role = ActorRole.Main or Supporting or Background, voiceActor =  Actor(name, image)) }
-            // this.comingSoon = comingSoon // Boolean
-            // this.syncData = syncData //
-            // this.posterHeaders = posterHeaders // Ex: mapOf<String, String>() Or network.CloudflareKiller.Intercepter.getCookieHeaders(mainUrl).toMap()
-            // this.nextAiring = nextAiring // NextAiring(episode: Int, unixTime: Long, season: Int)
-            // this.seasonNames = seasonNames // List<SeasonData> Ex: SeasonData(val season: Int, val name: String? = null, val displaySeason: Int? = null)
-            // this.backgroundPosterUrl = backgroundPosterUrl // String
-            // this.contentRating = contentRating // String
-        }
-    }
-
-    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
-        val document = app.get(data).document
-        val video_link = document.select("div.link iframe").map {
-            fixUrl( it.attr("src"))
-        }
-
-        for (video_map in video_link) {
-            loadExtractor(video_map, "${mainUrl}/", subtitleCallback, callback)
-        }
-     
-        return true
-    }
+    
 }
