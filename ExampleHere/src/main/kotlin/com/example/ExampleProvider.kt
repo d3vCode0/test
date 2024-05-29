9148bf16f664c39befc5fc7d3c76404add3aa5f1
devosto <gamosgame2@gmail.com>
2024-05-29 19:18:56 +0000

    Update ExampleProvider.kt

--- ExampleHere/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleHere/src/main/kotlin/com/example/ExampleProvider.kt
@@ -33,11 +33,31 @@ class ExampleAPi : MainAPI() {
     private fun Element.toSearchResponse(): SearchResponse? {
         val title = this.selectFirst("div.info h3")?.text()?.trim() ?: return null
         val href = this.selectFirst("a")?.attr("href") ?: return null
+        val posterUrl = this.selectFirst("a")?.attr("data-src") ?: return null
 
         Log.d("D3V title",title)
         Log.d("D3V title",href)
 
-        return newMovieSearchResponse(title, href, TvType.AnimeMovie) {
+        return newAnimeSearchResponse(title, href, TvType.Anime) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(request.data + page).document
+        if(document.select("title").text() == "Just a moment...") {
+            val document = app.get(request.data + page, interceptor = cfKiller, timeout = 120).document
+        }
+
+        val title     = document.selectFirst("div.head-box div.media-title h3")?.text()?.trim() ?: document.selectFirst("div.head-box div.media-title h1")?.text()?.trim() ?: return null
+        val bgImage   = fixUrlNull(document.selectFirst("div.banner")?.attr("data-src")) ?: return null
+        val posterUrl = fixUrlNull(document.selectFirst("div.anime-card div.image")?.attr("data-src")) ?: fixUrlNull(document.selectFirst("div.head-box div.banner")?.attr("data-src")) ?: return null
+        
+        Log.d("D3V > title", title)
+        Log.d("D3V > bg", bgImage)
+        Log.d("D3V > poster", posterUrl)
+        return newAnimeLoadResponse(title, url, TvType.Anime, true) {
+            this.posterUrl = posterUrl
         }
     }
 
