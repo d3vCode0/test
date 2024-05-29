259ca967b882c1b42c4c27e882103daba041eb70
d3vCode0 <168995040+d3vCode0@users.noreply.github.com>
2024-05-29 18:38:43 +0000

    Update ExampleProvider.kt

--- ExampleHere/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleHere/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,60 +1,44 @@
 package com.example
 
-import com.lagradost.cloudstream3.*
-import org.jsoup.nodes.Element
 
+import android.util.Log
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
+import com.lagradost.cloudstream3.network.CloudflareKiller
+import org.jsoup.nodes.Element
 
 class ExampleAPi : MainAPI() {
     override var mainUrl = "https://ww3.animerco.org"
-    override var name = "Example"
+    override var name = "Animerco"
     override val hasMainPage = true
-    override var lang = "en"
-    override val supportedTypes = setOf(
-        TvType.Anime,
-        TvType.AnimeMovie
-    )
+    override var lang = "ar"
+    override val supportedTypes = setOf(TvType.AnimeMovie, TvType.Anime)
+    private val cfKiller = CloudflareKiller()
 
     override val mainPage = mainPageOf(
-        "${mainUrl}/episodes/page/" to "Episodes",
-        //add more here
+        "${mainUrl}/animes/page/" to "Animes"
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get(request.data).document
-        val lists = document.select("div.page-content div.row div.col-12").mapNotNull {
+        val doc = app.get(request.data + page).document
+        if(doc.select("title").text() == "Just a moment...") {
+            doc = app.get(request.data + page, interceptor = cfKiller, timeout = 120).document
+        }
+        val home = doc.select("div.page-content .row div.box-5x1").mapNotNull {
             it.toSearchResponse()
         }
-        return newHomePageResponse(
-            HomePageList(
-            name = request.name,
-            lists,
-            isHorizontalImages = true
-            ))
+        return newHomePageResponse(request.name, home)
     }
-    
-    private fun Element.toSearchResponse(): SearchResponse? {
-        val title = this.selectFirst("div.title h1")?.text()?.trim() ?: return null
-        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = this.selectFirst("a")?.attr("data-src") ?: return null
 
-        return newAnimeSearchResponse(title, href, TvType.Anime) {
-            // this.name = name // String
-            // this.url = url // String
-            // this.apiName = apiName // String
-            // this.type = TvType.Anime // TvType
-            this.posterUrl = posterUrl // String
-            // this.year = year //Int
-            // this.dubStatus = dubStatus // Ex: EnumSet.of(DubStatus.None, DubStatus.Dubbed, DubStatus.Subbed)
-            // this.otherName = otherName // String
-            // this.episodes = episodes // Ex: MutableMap<String, Int> = MutableMapOf(DubStatus.None, 0)
-            // this.id = id // Int
-            // this.quality = quality // Ex: SearchQuality.HD
-            // this.posterHeaders = posterHeaders // Ex: mapOf<String, String>() Or network.CloudflareKiller.Intercepter.getCookieHeaders(mainUrl).toMap()
+    private fun Element.toSearchResponse(): SearchResponse? {
+        val title = this.selectFirst("div.info h3")?.text()?.trim() ?: return null
+        val href = this.selectFirst("a")?.attr("href") ?: return null
+
+        Log.d("D3V title",title)
+        Log.d("D3V title",href)
+
+        newMovieSearchResponse(title, href, TvType.AnimeMovie) {
         }
     }
 
-
-    
-
-    
 }
